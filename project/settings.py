d2a9036ee23b0e94d1e3b86e7b9cf99bf3336615
Bruno Pupo <brunopupo1@gmail.com>
2026-03-11 19:59:23 -0300

    Criei o arquivo local_settings.py de onde são importadas as configurações para o settings.py

--- project/settings.py
+++ project/settings.py
@@ -129,3 +129,13 @@ STATIC_ROOT = BASE_DIR / 'static'  # collectstatic
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+
+
+
+
+try:
+    from project.local_settings import *
+except ImportError:
+    ...
