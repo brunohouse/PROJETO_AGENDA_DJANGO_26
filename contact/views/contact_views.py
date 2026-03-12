313736be3e8a068fb3fb5a51a9fd00327cc8a208
Bruno Pupo <brunopupo1@gmail.com>
2026-03-11 21:44:29 -0300

    FIM DA AULA 470 - adicionei a query de contacts .filter() e .order_by()

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -4,11 +4,14 @@ from contact.models import Contact
 # Create your views here.
 
 def index(request):
-    contacts = Contact.objects.all()
-
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .order_by('-id')[0:10]
+    
+    # print(contacts.query)  mostra a query(caminho) que o programa percorre e solicita no banco de dados
+    
     context = {
         'contacts': contacts,
-
     }
 
     return render(
