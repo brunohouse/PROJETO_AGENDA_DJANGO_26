52a88f05b532971db4f14dc3d636f9d780861029
Bruno Pupo <brunopupo1@gmail.com>
2026-03-14 11:08:15 -0300

    FIM DA AULA 478 - Criando a URL, view e template para criar um contato (CREATE)

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404 , redirect
 from django.db.models import Q
 from contact.models import Contact
-from django.http import Http404
 from django.core.paginator import Paginator
 
 
